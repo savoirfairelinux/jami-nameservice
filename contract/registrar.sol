24aa2a0c92cb4fb86d9ed8845d92194367f8db0b
felixsidokhine <felix.sidokhine@savoirfairelinux.com>
2019-03-05 14:30:22 -0500

    nameservice: public-key and signature now stored on blockchain
    The public-key and signature can now be POSTed at time of registration
    These are validated by server before posting to blockchain, and can be
    retrieved by users for validation on their local machines as well
    
    Added appropriate HTTP codes as well
    
    Modified contract to store and retrieve these values and various scripts
    (server, batch reading/writing) to handle these new values
    
    Change-Id: Iddcaa00ab0d7068cea6d578489eacb89e8354b7c

--- contract/registrar.sol
+++ contract/registrar.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.2;
 /*
  * Copyright (c) 2014 Gav Wood <g@ethdev.com>
  * Copyright (c) 2016 Savoir-faire Linux Inc.
@@ -26,24 +26,26 @@ pragma solidity ^0.4.0;
  */
 
 contract NameRegister {
-  function addr(bytes32 _name) constant returns (address o_owner) {}
-  function name(address _owner) constant returns (bytes32 o_name) {}
+  function addr(bytes32 _name) public view returns (address o_owner) {}
+  function name(address _owner) public view returns (bytes32 o_name) {}
 }
 
 contract Registrar is NameRegister {
   event Changed(bytes32 indexed name);
   event PrimaryChanged(bytes32 indexed name, address indexed addr, address owner);
 
-  function owner(bytes32 _name) constant returns (address o_owner) {}
-  function addr(bytes32 _name) constant returns (address o_address) {}
-  function subRegistrar(bytes32 _name) constant returns (address o_subRegistrar) {}
-  function content(bytes32 _name) constant returns (bytes32 o_content) {}
+  function owner(bytes32 _name) public view returns (address o_owner) {}
+  function addr(bytes32 _name) public view returns (address o_address) {}
+  function subRegistrar(bytes32 _name) public view returns (address o_subRegistrar) {}
+  function content(bytes32 _name) public view returns (bytes32 o_content) {}
 
-  function name(address _owner) constant returns (bytes32 o_name) {}
+  function name(address _owner) public view returns (bytes32 o_name) {}
 }
 
 contract GlobalRegistrar is Registrar {
   struct Record {
+    string publicKey;
+    string signedName;
     address owner;
     address primary;
     address subRegistrar;
@@ -52,68 +54,70 @@ contract GlobalRegistrar is Registrar {
     uint renewalDate;
   }
 
-  function Registrar() {
-  }
-
-  function reserve(bytes32 _name, address _a) {
-    if (m_toRecord[_name].owner == 0 && m_toName[_a] == 0) {
+  function reserve(bytes32 _name, address _a) public {
+    if (m_toRecord[_name].owner == address(0) && m_toName[_a] == 0) {
       m_toRecord[_name].owner = msg.sender;
       m_toRecord[_name].primary = _a;
       m_toName[_a] = _name;
-      Changed(_name);
-      PrimaryChanged(_name, _a, msg.sender);
+      emit Changed(_name);
+      emit PrimaryChanged(_name, _a, msg.sender);
     }
   }
-  function reserveFor(bytes32 _name, address _owner, address _a) {
-    if (m_toRecord[_name].owner == 0 && m_toName[_a] == 0) {
+  function reserveFor(bytes32 _name, address _owner, address _a, string memory _publickey, string memory _signedname) public {
+    if (m_toRecord[_name].owner == address(0) && m_toName[_a] == 0) {
       m_toRecord[_name].owner = _owner;
       m_toRecord[_name].primary = _a;
+      m_toRecord[_name].publicKey = _publickey;
+      m_toRecord[_name].signedName = _signedname;
+
       m_toName[_a] = _name;
-      Changed(_name);
-      PrimaryChanged(_name, _a, _owner);
+      emit Changed(_name);
+      emit PrimaryChanged(_name, _a, _owner);
     }
   }
 
   modifier onlyrecordowner(bytes32 _name) { if (m_toRecord[_name].owner == msg.sender) _; }
 
-  function transfer(bytes32 _name, address _newOwner) onlyrecordowner(_name) {
+  function transfer(bytes32 _name, address _newOwner) public onlyrecordowner(_name) {
     m_toRecord[_name].owner = _newOwner;
-    Changed(_name);
+    emit Changed(_name);
   }
 
-  function disown(bytes32 _name) onlyrecordowner(_name) {
+  function disown(bytes32 _name) public onlyrecordowner(_name) {
     if (m_toName[m_toRecord[_name].primary] == _name)
     {
-      PrimaryChanged(_name, m_toRecord[_name].primary, m_toRecord[_name].owner);
+      emit PrimaryChanged(_name, m_toRecord[_name].primary, m_toRecord[_name].owner);
       m_toName[m_toRecord[_name].primary] = "";
     }
     delete m_toRecord[_name];
-    Changed(_name);
+    emit Changed(_name);
   }
 
-  function setAddress(bytes32 _name, address _a, bool _primary) onlyrecordowner(_name) {
+  function setAddress(bytes32 _name, address _a, bool _primary) public onlyrecordowner(_name) {
     m_toRecord[_name].primary = _a;
     if (_primary)
     {
-      PrimaryChanged(_name, _a, m_toRecord[_name].owner);
+      emit PrimaryChanged(_name, _a, m_toRecord[_name].owner);
       m_toName[_a] = _name;
     }
-    Changed(_name);
+    emit Changed(_name);
   }
-  function setSubRegistrar(bytes32 _name, address _registrar) onlyrecordowner(_name) {
+  function setSubRegistrar(bytes32 _name, address _registrar) public onlyrecordowner(_name) {
     m_toRecord[_name].subRegistrar = _registrar;
-    Changed(_name);
+    emit Changed(_name);
   }
-  function setContent(bytes32 _name, bytes32 _content) onlyrecordowner(_name) {
+  function setContent(bytes32 _name, bytes32 _content) public onlyrecordowner(_name) {
     m_toRecord[_name].content = _content;
-    Changed(_name);
+    emit Changed(_name);
   }
 
-  function owner(bytes32 _name) constant returns (address) { return m_toRecord[_name].owner; }
-  function addr(bytes32 _name) constant returns (address) { return m_toRecord[_name].primary; }
-  function register(bytes32 _name) constant returns (address) { return m_toRecord[_name].subRegistrar; }
-  function content(bytes32 _name) constant returns (bytes32) { return m_toRecord[_name].content; }
-  function name(address _a) constant returns (bytes32 o_name) { return m_toName[_a]; }
+  function owner(bytes32 _name) public view returns (address) { return m_toRecord[_name].owner; }
+  function addr(bytes32 _name) public view returns (address) { return m_toRecord[_name].primary; }
+  function register(bytes32 _name) public view returns (address) { return m_toRecord[_name].subRegistrar; }
+  function content(bytes32 _name) public view returns (bytes32) { return m_toRecord[_name].content; }
+  function name(address _a) public view returns (bytes32 o_name) { return m_toName[_a]; }
+  function publickey(bytes32 _name) public view returns (string memory) { return m_toRecord[_name].publicKey; }
+  function signature(bytes32 _name) public view returns (string memory) { return m_toRecord[_name].signedName; }
 
   mapping (address => bytes32) m_toName;
   mapping (bytes32 => Record) m_toRecord;
